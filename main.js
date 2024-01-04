da97cbd6dfa4b5a53a3c6f02aeffe43b5d268b5b
Davey-Seeman <daveyseemanma@gmail.com>
2024-01-03 21:35:38 -0500

    Fixed to accomodate Firebase updates

---
+++ main.js
@@ -0,0 +1,269 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getDatabase, ref, onValue, set, push, query,orderByKey,onChildAdded,update} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD_ziOsM9pR1hPixreijav-bry7MnDYyBQ",
+    authDomain: "ideanet-645b1.firebaseapp.com",
+    projectId: "ideanet-645b1",
+    storageBucket: "ideanet-645b1.appspot.com",
+    messagingSenderId: "504272300823",
+    appId: "1:504272300823:web:3f9d8074bc9193713d6552",
+    measurementId: "G-Y1K8448V23"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+var ideas = ref(database,"Ideas");
+var oldDataHold = {};
+var dataHold = {};
+onValue(ideas, (snapshot) => {
+    const data = snapshot.val();
+    gotList(data,dataHold);
+});
+
+reset();
+
+
+function reset(){ //resets variables to be reused in sorting functions
+    document.getElementById("postSection").innerHTML = "";
+    try{
+       ideaNumber = counter = 0;
+       likedAlrdy = reportedAlrdy = false
+    }catch(error){};
+};
+
+function addData(){
+    var ideaNumber = Object.values(oldDataHold).length;
+
+    var tit = document.getElementById("titleInput").value;
+    var des = document.getElementById("descriptionInput").value;
+    var aud = document.getElementById("audienceInput").value;
+    var imp = document.getElementById("importanceInput").value;
+    var img = document.getElementById("imageInput").value;
+    var tags = document.getElementById("tagInput").value;
+
+    var tagArr = tags.split(" ");
+    var tagObj = {};
+
+    for (var i = 0; i < tagArr.length; i++){ //contstruct obj containing all inputted tags w each word from input
+        if(tagArr[i]){ //only add to database if tag is truthy
+            tagObj[i] = tagArr[i]; //tag index system may skip values if user inputs multiple spaces in a row, however this does not affect the program
+        };
+    };
+
+    if(tit.length == 0 || des.length == 0 || aud.length == 0 || imp.length == 0){  //end function if not all input boxes are full on idea submit page
+        alert("Please input in all fields");
+        return;
+    };
+
+    if(tit.split(' ').length > 9) { //check if title is too long (part of success criteria 1)
+        alert("Your title is too long");
+        return;
+    };
+
+    if (img.match(/\.(jpeg|jpg|gif|png)$/) == null){  //also check if image is invalid seperatedly as it is a success criteria
+
+        //case where img is invalid
+        if(confirm("You are sumbitting a post without an image, is that ok?") == false){ //confirmation
+                return;
+        };
+    };
+
+    //function will only continue if url is valid or user clicked "Ok" in response to the invalid image confirmation
+
+    var data = //Idea data
+        {
+            title: tit,
+            description: des,
+            targetAudience: aud,
+            importance: imp,
+            imageURL: img,
+            likes: 0,
+            reports: 0,
+            tags: tagObj,
+            comments: 0
+        }
+    set(push(ref(database, 'Ideas')), {
+        data
+    });
+    //ref.child('Idea' + (ideaNumber+1)).set(data);
+    postModal.style.display = "none";
+
+    data.tags = tagArr; //for data passed into onscreen addPost function
+
+    alert("Your Idea Has Been Posted... Thank You!");
+    window.location.reload();
+};
+
+function addLike(currentPost,likesNumber){
+    update(ref(database,'Ideas/'+currentPost.getAttribute("key")+'/data'),{
+        likes: likesNumber + 1
+    })
+
+    document.getElementById("likesCount" + currentPost.id.match(/\d+/g)[0]).innerHTML = likesNumber + 1 //add 1 to the on screen like count //use id no matter what for onscreen changes
+
+    document.getElementById("likesPost" + currentPost.id.match(/\d+/g)[0]).innerHTML = likesNumber + 1 //add 1 to the browsing page on-hover like count
+};
+
+function reported(currentPost){ //adds report to database
+    alert("Thank you for reporting this idea. We will review it soon.");
+    /*reportNumber = currentPost.id.match(/\d+/g)[0]; //regex to grab # from id
+
+    increment = database.ref("Ideas/Idea" + reportNumber);
+
+    if(reportedAlrdy == false){ //check if post has already been reported
+        newReported = reportNumber + 1;
+        increment.update({reports: newReported}); //increment report number by 1
+        alert("Your report has been recorded");
+        reportedAlrdy = true;
+
+        //remove post from firebase database if the number of reports are equal to or greater than 1/5 of the number of likes:
+
+        likesNumber = dataHold["Idea" + reportNumber]["likes"];
+
+        if (likesNumber/5 <= newReported){
+            //increment.remove(); //this line removes idea from firebase database. However instead of removing the idea from the database I will add the post to a "deletable" section of the database so that I (the client) could review whether it should be taken down or the report button was mistakenly pressed: (the remove() feature will be used if my website gets too populated to monitor)
+
+            deletableSection = database.ref("Deletable");
+            title = document.getElementById("title" + reportNumber);
+            deletableSection.push("Delete Post: " + title.innerHTML);
+        };
+    };*/
+
+};
+
+function showSuggested(){
+
+    document.getElementById("suggestions").style.display = "block"; //make visible
+
+    document.getElementById("suggestions").innerHTML = " "; //clear suggestions 
+
+    //the following code searches the firebase database to find tags that match the input:
+
+    ref.once("value", searchTags, errData);
+};
+
+function searchTags(data){
+    data = data.val();
+
+    input = document.getElementById("searchInput").value;
+    suggestionCounter = 0; //used limit # of tag suggestions to 4
+    suggestionList = []; //to make sure the program does not show the same suggestion twice
+
+    baseLoop:
+    for (const idea in data){ //loops through ideas
+        for (const tags in data[idea]["tags"]){ //loops through all tags on each idea
+
+            singleTag = data[idea]["tags"][tags].toLowerCase(); //keep everything lower case
+
+            if (singleTag.includes(input.toLowerCase())){ //check if input is contained in any tags
+                if (suggestionList.includes(singleTag)){ //make sure same tag cannot be shown twice
+                    continue;
+                };
+                suggestionList.push(singleTag);
+                newSuggestion = document.createElement("button");
+                newSuggestion.appendChild(document.createTextNode(singleTag));
+                newSuggestion.setAttribute("id","tagName: " + singleTag);
+                newSuggestion.setAttribute("onclick","sortTagged(this)");
+                document.getElementById("suggestions").appendChild(newSuggestion);
+                suggestionCounter ++;
+                if (suggestionCounter > 3) { //maximize suggestionCounter to 4
+                    break baseLoop;
+                };
+            };
+        };
+    };
+};
+
+function hideSuggested(){
+    document.getElementById("suggestions").style.display = "none";
+};
+
+function addComment(currentPost){
+
+    /*try { //comment section number is based on class name IF it was called after a sorting function. Otherwise null
+        var commentNumber = currentPost.className.match(/\d+/g)[0];  //regex to grab # from id
+    } catch { //If addLike is called directly from original posting function
+        var commentNumber = currentPost.id.match(/\d+/g)[0]; //regex to grab # from id
+    };*/
+    comment = document.getElementById("commentTxt" + currentPost.getAttribute("key")).value;
+
+    if(comment.length){
+        /*
+        var onScreenComments = document.getElementById("commentList" + commentNumber);
+
+        var commentEntry = document.createElement("p");
+        commentEntry.className = "idea-comments";
+        commentEntry.appendChild(document.createTextNode(comment));
+
+        onScreenComments.appendChild(commentEntry); //add comment onscreen 
+
+        //add <br> line breaks to comment based on how many lines comment is to avoid overlap over other comments
+
+        if (comment.length > 45){ //there are approximately 45 characters in a line
+            approxLineNumber = String(comment.length/45).charAt(0);
+            linebreak = document.createElement("br");
+            for (j = 0; j < approxLineNumber; j++){
+                onScreenComments.appendChild(linebreak); //add line break approxLineNumber times
+            };
+        };*/
+
+       push(ref(database,'Ideas/'+currentPost.getAttribute("key")+'/data/comments'),comment)
+       //updateKey = database.ref("Ideas/Idea" + commentNumber + "/comments");
+       //updateKey.push(comment); //add to database*/
+
+    } else {
+
+        alert("No comment has been inputted");
+
+};
+};
+function gotList(allData,dataHold) //shows all the ideas on the browsing feed
+    {	
+    var postDisplay = document.getElementById("postSection").innerHTML = ""; //clear all posts (useful for when function is recalled in "clear tags" button)
+
+    //allData = data.val() //val function turns data into dictionary
+    var keyValues = Object.keys(allData);
+    console.log(allData)
+
+    var multiDigitPosts = [];
+
+    if(keyValues.length > 9)
+    { //adjust array to work with multi digit numbers (in firebase ordering/indexing does not work w more than 10 posts)
+        for(key in allData){ //allData must be reordered so that multi digit posts come after single digit posts; unfortunately in firebase idea10 comes before idea2 for example
+        		
+            keyNumber = key.match(/\d+/g)[0]; //regex finds number in key
+            if(keyNumber >= 10){ //check if key is multiple digits.
+                multiDigitPosts.push(key); //add multidigit value into array
+                keyValues.splice(keyValues.indexOf(key), 1); //delete multidigit value from keyValues (it is to be added back to the end though)
+            };
+        };
+        keyValues.push(...multiDigitPosts); //use of spread operator to put multiDigitPosts at the end of single digit posts
+    };
+
+
+    var ideaNumber = 1
+    for(const [key, value] of Object.entries(allData)){
+        showPost(value,ideaNumber,key)
+        ideaNumber++
+    }
+
+        /*for (i = 0; (i < keyValues.length) && (i < 18); i++) //this will either draw 18 posts or all the posts in my database (only if it is under 18), if 18 posts is too laggy I could change this number (keyValues.length) to be less
+        {   
+        	keyName = "Idea" + (i+1);
+        	allData[keyName]["number"] = (i+1); //variable is used for adding likes after tag sort
+        	dataHold[keyValues[i]] = allData[keyName]; //dataHold should be sorted in the same way as keyValues and only be the length of the # of posts being displayed (this is why it is put in the for loop)
+            //var databaseIdeas = ref(database,"Ideas/" + keyValues[i]);
+            //databaseIdeas.once("value", showPost, errData);
+            console.log(allData[keyName])
+            showPost(allData[keyName],i+1,dataHold)
+        };*/
+
+        oldDataHold = dataHold;
+};
+
+export { addData, reset, addLike, reported, addComment };
--- posting.js
+++ posting.js
@@ -1,54 +1,13 @@
-	function gotList(data) //shows all the ideas on the browsing feed
-    {	
-    	postDisplay = document.getElementById("postSection").innerHTML = ""; //clear all posts (useful for when function is recalled in "clear tags" button)
-
-        allData = data.val() //val function turns data into dictionary
-        keyValues = Object.keys(allData);
-
-        multiDigitPosts = [];
-
-       if(keyValues.length > 9)
-       { //adjust array to work with multi digit numbers (in firebase ordering/indexing does not work w more than 10 posts)
-        	for(key in allData){ //allData must be reordered so that multi digit posts come after single digit posts; unfortunately in firebase idea10 comes before idea2 for example
-        		
-        		keyNumber = key.match(/\d+/g)[0]; //regex finds number in key
-        		if(keyNumber >= 10){ //check if key is multiple digits.
-        			multiDigitPosts.push(key); //add multidigit value into array
-					keyValues.splice(keyValues.indexOf(key), 1); //delete multidigit value from keyValues (it is to be added back to the end though)
-        		};
-        	};
-        	keyValues.push(...multiDigitPosts); //use of spread operator to put multiDigitPosts at the end of single digit posts
-        };
-
-
-        for (i = 0; (i < keyValues.length) && (i < 18); i++) //this will either draw 18 posts or all the posts in my database (only if it is under 18), if 18 posts is too laggy I could change this number (keyValues.length) to be less
-        {   
-        	keyName = "Idea" + (i+1);
-        	allData[keyName]["number"] = (i+1); //variable is used for adding likes after tag sort
-        	dataHold[keyValues[i]] = allData[keyName]; //dataHold should be sorted in the same way as keyValues and only be the length of the # of posts being displayed (this is why it is put in the for loop)
-            databaseIdeas = database.ref("Ideas/" + keyValues[i]);
-            databaseIdeas.once("value", showPost, errData);
-        };
-
-        oldDataHold = dataHold;
-    };
-
-    function errData(err)
-    {
-        console.log("Error!")
-        console.log(err)
-  	}
-
-    function showPost (data)
+function showPost(data,ideaNumber,key)
     {	
     	try { //if .val() function is possible (initial post display)
-   			fullData = data.val();
+   			fullData = data.data;
 		} catch { //if .val() function is impossible (after sorting functions)
-			fullData = data;
+            fullData = data;
 		};
+        
 
-    	ideaNumber ++;
-
+    	//ideaNumber++;
      	browsingData = [fullData.title, fullData.description, fullData.imageURL]; //data that is shown on the browsing page
         if (browsingData[1].length > 120) {
         	browsingData[1] = browsingData[1].substring(0, 120) + "..."; //limit # of characters on browsing page description
@@ -81,7 +40,7 @@
 		postBox.appendChild(heartIcon);
 		likes = document.createElement("p");
 		likes.setAttribute("id", "likesPost" + ideaNumber);
-		likes.appendChild(document.createTextNode(dataHold["Idea" + ideaNumber]["likes"])); //description data list not used for this line as it resulted in inconsistencies when used. //you need to put this text in a <p> because text nodes cannot be styled on their own
+		likes.appendChild(document.createTextNode(fullData.likes)); //description data list not used for this line as it resulted in inconsistencies when used. //you need to put this text in a <p> because text nodes cannot be styled on their own
 		likes.className = "likesTxt";
 		postBox.appendChild(likes);
 
@@ -126,13 +85,15 @@
 		likesIcon.className = "fa fa-heart";
 		likesIcon.style.visibility = "visible";
 		likesIcon.setAttribute("id","likesOnPost" + ideaNumber);
+        likesIcon.setAttribute("key",key)
 
-		likesIcon.setAttribute("onclick","addLike(this);");
+        console.log(key)
+		likesIcon.setAttribute("onclick", "import('./main.js').then(module => module.addLike(this,fullData.likes))");
 		header.appendChild(likesIcon);
 		likesText = document.createElement("p");
 		likesText.setAttribute("id", "likesCount" + ideaNumber);
 		likesText.className = "likes-number";
-		likesText.appendChild(document.createTextNode(dataHold["Idea" + ideaNumber]["likes"])); //description data list not used for this line as it resulted in inconsistencies when used.
+		likesText.appendChild(document.createTextNode(fullData.likes)); //description data list not used for this line as it resulted in inconsistencies when used.
 		header.appendChild(likesText);
 
 		body = document.createElement("div");
@@ -171,7 +132,7 @@
 		reportBtn.className = "fa fa-exclamation-triangle";
 		reportBtn.appendChild(document.createTextNode("Report "));
 		reportBtn.setAttribute("id", "reportButton" + ideaNumber);
-		reportBtn.setAttribute("onclick", "reported(this);");
+		reportBtn.setAttribute("onclick", "import('./main.js').then(module => module.reported(this))");
 		body.appendChild(reportBtn);
 
 		tagsIcon = document.createElement("i"); //add tags to body (not header because it has seperate styling)
@@ -182,18 +143,20 @@
 
 		if (descriptionData[5]){ //check if entered tags are empty
 
-			tagsText = descriptionData[5].join(", ").replace(/,\s*$/, ""); //turns list of tags into string w vals separated by commas. regex gets rid of trailing comma and any extra spaces
+			var tagsText = descriptionData[5].join(", ").replace(/,\s*$/, ""); //turns list of tags into string w vals separated by commas. regex gets rid of trailing comma and any extra spaces
 			if  (tagsText.length > 55){
 				tagsText = tagsText.substring(0, 55) + "..."; //limit to only 55 chars to make sure it will fit in the header
 			};
 
 		};
-
+        try{
 		tags.appendChild(document.createTextNode(tagsText));
 		body.appendChild(tags);
+        }catch{}
 
 		commentSec = document.createElement("div");
-		commentSec.setAttribute("id", "commentList" + ideaNumber);
+		//commentSec.setAttribute("id","commentSec" + ideaNumber);
+        //commentSec.setAttribute("name","com"+key);
 		commentSec.className = "comment-section";
 		body.appendChild(commentSec);
 		commentTitle = document.createElement("p");
@@ -202,8 +165,9 @@
 		body.appendChild(commentTitle);
 
 		//adding comments to post:
-		databaseComments = database.ref("Ideas/" + "Idea" + ideaNumber + "/comments");
-		databaseComments.once("value", gotComments, errData);
+        commentDisplay(Object.values(fullData.comments),commentSec)
+		//databaseComments = ref(database,"Ideas/" + "Idea" + ideaNumber + "/comments");
+		//databaseComments.once("value", gotComments, errData);
 
 		content.appendChild(body);
 
@@ -214,7 +178,7 @@
 		inputCom = document.createElement("input");
 		inputCom.className = "commentInput";
 		inputCom.setAttribute("type", "text");
-		inputCom.setAttribute("id", "commentTxt" + ideaNumber);
+		inputCom.setAttribute("id", "commentTxt" + key);
 		inputCom.setAttribute("placeholder", "Add a comment...");
 		footer.appendChild(inputCom);
 		submitBtn = document.createElement("submit");
@@ -226,34 +190,36 @@
 		submitBtn.setAttribute("id", "commentSection" + ideaNumber);
 
 		submitBtn.appendChild(submitIcon);
-		submitBtn.setAttribute("onclick", "addComment(this)");
+        submitBtn.setAttribute("key",key);
+        submitBtn.setAttribute("onclick", "import('./main.js').then(module => module.addComment(this))");
 
 		page.append(footer);
 
 		postDisplay.appendChild(page);
     };
 
-    function gotComments(data){ //intial comment display on posts
-    	counter ++;
-		commentData = data.val();
+    /*function gotComments(data){ //intial comment display on posts
+    	//counter ++;
 
-		allComments = Object.values(commentData);
+		allComments = Object.values(data);
+        console.log(allComments)
 
-		commentDisplay(allComments, counter); //put comment drawing in a seperate function because it will have to be called multiple times (e.g. in sorting functions)
-	};
+		commentDisplay(allComments); //put comment drawing in a seperate function because it will have to be called multiple times (e.g. in sorting functions)
+	};*/
 
-	function commentDisplay(allComments, arrayIndex){ //function for whenever comments are displayed
+	function commentDisplay(allComments,ideaComments){ //function for whenever comments are displayed
 
-	    ideaComments = document.getElementById("commentList" + arrayIndex);
+	    //var ideaComments = document.getElementById("commentSec" + ideaNumber);
+        
 
-	    if(ideaComments.hasChildNodes() || !allComments.length){ //check for no comments or if comments were already drawn (e.g. in a sorting function)
+	    /*if(ideaComments.hasChildNodes() || !allComments.length){ //check for no comments or if comments were already drawn (e.g. in a sorting function)
 	        
 	        if(!allComments.length){
 	        	ideaComments.appendChild(document.createTextNode("")); //add empty text node to avoid glitch where other posts' comments add to uncommented post
 	        };
 
 	        return;
-	    };
+	    };*/
 
 	    for (j = 0; j < allComments.length; j++){
 		    comment = document.createElement("p");
